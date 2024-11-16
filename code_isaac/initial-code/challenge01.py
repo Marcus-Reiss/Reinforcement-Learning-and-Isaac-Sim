30632d06c00da2fe61473ed56a70fb9158c9e7d7
Marcus-Reiss <marcusreispestalozzi2@gmail.com>
2024-11-16 15:01:15 -0300

    finishing changes in challenge01.py

--- code_isaac/initial-code/challenge01.py
+++ code_isaac/initial-code/challenge01.py
@@ -18,7 +18,7 @@ import torch
 import omni.isaac.core.utils.prims as prim_utils
 import omni.isaac.lab.sim as sim_utils
 import omni.isaac.lab.utils.math as math_utils
-from omni.isaac.lab.assets import RigidObject, RigidObjectCfg
+from omni.isaac.lab.assets import RigidObject, RigidObjectCfg, DeformableObject, DeformableObjectCfg
 from omni.isaac.lab.sim import SimulationContext
 
 
@@ -61,18 +61,24 @@ def setup_scene():
     cone_object = RigidObject(cfg=cone_cfg)
 
     # Adding a blue deformable cuboid
-    cfg_cuboid_deformable = sim_utils.MeshCuboidCfg(
+    cuboid_cfg = DeformableObjectCfg(
+        prim_path="/World/Origin4/Cube",
+        spawn=sim_utils.MeshCuboidCfg(
         size=(0.5, 0.5, 0.5),
         deformable_props=sim_utils.DeformableBodyPropertiesCfg(),
         visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 0.0, 1.0)),
         physics_material=sim_utils.DeformableBodyMaterialCfg(),
+        ),
+        init_state=DeformableObjectCfg.InitialStateCfg(),
+        debug_vis=True,
     )
-    cfg_cuboid_deformable.func("/World/Origin4/CuboidDeformable", cfg_cuboid_deformable, translation=origins[3])
+    # cfg_cuboid_deformable.func("/World/Origin4/CuboidDeformable", cfg_cuboid_deformable, translation=origins[3])
+    cuboid_object = DeformableObject(cfg=cuboid_cfg)
 
-    return cone_object, origins
+    return cone_object, cuboid_object, origins
 
 
-def run_simulation(sim: sim_utils.SimulationContext, entities: RigidObject, origins: torch.Tensor):
+def run_simulation(sim: sim_utils.SimulationContext, entities: RigidObject, cuboid: DeformableObject, origins: torch.Tensor):
     """ Include here all code for running the simulation """
     sim_dt = sim.get_physics_dt()
     sim_time = 0.0
@@ -82,28 +88,41 @@ def run_simulation(sim: sim_utils.SimulationContext, entities: RigidObject, orig
 
         if count % 250 == 0:
             
-            # 1. Get the default state for each object
+            # 1. Get the default state for each object (cones)
             root_state = entities.data.default_root_state.clone()
+
+            # 1.2. Get the default state for cuboid object
+            nodal_state = cuboid.data.default_nodal_state_w.clone()
 
             # 2. Change the position of cones for matching each origin
             root_state[:, :3] += torch.tensor(origins[:3]) + math_utils.sample_cylinder(
-            radius=0.1, h_range=(1.0, 2.0),
-            size=3,
+            radius=0.1, h_range=(3.0, 3.5),
+            size=entities.num_instances,
             device=entities.device
             )
 
             # Trying to do the same for the cuboid
-            # root_state[:, :3] += torch.tensor(origins[3]) + math_utils.sample_cylinder(
-            # radius=0.1, h_range=(1.0, 2.0),
-            # size=1,
-            # device=entities.device
-            # )
+            # pos_w = torch.rand(cuboid.num_instances, 3, device=sim.device)*0.8 + origins[3]
+            pos_w = torch.tensor(origins[3], device=sim.device)
+
+            random_offset = math_utils.sample_cylinder(
+            radius=0.2, h_range=(2.0, 2.5),
+            size=cuboid.num_instances,
+            device=sim.device
+            )
+
+            pos_w += random_offset[0, :3]
+            # quat_w = math_utils.random_orientation(cuboid.num_instances, device=sim.device)
+            nodal_state[..., :3] = cuboid.transform_nodal_pos(nodal_state[..., :3], pos_w)
 
             # 3. Write root state to simulation
             entities.write_root_state_to_sim(root_state)
+            cuboid.write_nodal_state_to_sim(nodal_state)
 
             # 4. Reset buffers
             entities.reset()
+            cuboid.reset()
+
 
         entities.update(sim_dt)
 
@@ -123,7 +142,7 @@ def main():
     sim.set_camera_view(eye=[1.5, 0.0, 1.0], target=[0.0, 0.0, 0.0])
     
     # Design scene
-    scene_entities, scene_origins = setup_scene()
+    scene_entities, scene_cuboid, scene_origins = setup_scene()
     scene_origins = torch.tensor(scene_origins, device=sim.device)
     
     # Play the simulator
@@ -133,7 +152,7 @@ def main():
     print("[INFO]: Setup complete...")
     
     # Run the simulator
-    run_simulation(sim, scene_entities, scene_origins)
+    run_simulation(sim, scene_entities, scene_cuboid, scene_origins)
 
 if __name__ == "__main__":
     # run the main function
